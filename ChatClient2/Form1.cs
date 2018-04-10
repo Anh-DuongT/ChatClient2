2276fdae004ed2d3c95df5531f5073f3ee85c3d0
DELL 5448 <DELL 5448@DELL>
2018-04-10 23:21:09 +0700

    buoi5

--- ChatClient2/Form1.cs
+++ ChatClient2/Form1.cs
@@ -5,16 +5,43 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ChatClient2
 {
     public partial class Form1 : Form
     {
+        Socket client;
+        byte[] data;
+        IPEndPoint ipServer;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ipServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.Connect(ipServer);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox2.Text;
+            data = new byte[1024];
+            data = Encoding.ASCII.GetBytes(text);
+            listBox1.Items.Add("Client: " + text);
+            textBox2.Text = "";
+            client.Send(data);
+            data = new byte[1024];
+            client.Receive(data);
+            text = Encoding.ASCII.GetString(data);
+            listBox1.Items.Add(text);
         }
     }
 }
--- Server/Form1.Designer.cs
+++ Server/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ChatClient2
+﻿namespace Server
 {
     partial class Form1
     {
@@ -28,72 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(261, 17);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 13;
-            this.button2.Text = "Connect";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(31, 254);
-            this.textBox2.Multiline = true;
+            this.textBox2.Location = new System.Drawing.Point(106, 11);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(319, 65);
-            this.textBox2.TabIndex = 12;
+            this.textBox2.Size = new System.Drawing.Size(195, 20);
+            this.textBox2.TabIndex = 11;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(99, 20);
+            this.textBox1.Location = new System.Drawing.Point(22, 226);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(145, 20);
-            this.textBox1.TabIndex = 11;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 229);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 13);
-            this.label2.TabIndex = 10;
-            this.label2.Text = "Message";
+            this.textBox1.Size = new System.Drawing.Size(296, 60);
+            this.textBox1.TabIndex = 10;
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(31, 65);
+            this.listBox1.Location = new System.Drawing.Point(22, 48);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(319, 147);
+            this.listBox1.Size = new System.Drawing.Size(296, 147);
             this.listBox1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Message";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 23);
+            this.label1.Location = new System.Drawing.Point(56, 14);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "IP Server";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "IP Client";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(371, 252);
+            this.button1.Location = new System.Drawing.Point(342, 226);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 67);
-            this.button1.TabIndex = 7;
+            this.button1.Size = new System.Drawing.Size(75, 62);
+            this.button1.TabIndex = 6;
             this.button1.Text = "Send";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -102,16 +91,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(472, 351);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(454, 306);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Client";
+            this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,11 +107,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
     }
